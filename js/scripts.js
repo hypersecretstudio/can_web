d312c75f048df990bc30d3a155e9c9e5923b0cf1
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2019-04-21 16:59:39 +0200

    update script with text scrambler

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
+const scramble_text = (el) => {
+    // console.log(el.innerText);
+    const words = el.innerText.split(' ');
+    el.innerText = '';
+    const iterations = words.length;
+    let result_text = ''
+    for (let i = 0; i < iterations; i++) {
+        const random_idx = Math.floor(Math.random() * words.length);
+        const word = words[random_idx];
+        result_text += word + ' ';
+        words.splice(random_idx, 1);
+    }
+    el.innerText = result_text;
+}
+
 const reset_visibility = (children) => {
     for (const kid of children) {
         for (const grand_kid of kid.children) {
 
+            scramble_text(grand_kid)
             grand_kid.style.display = 'block';
         }
     }
